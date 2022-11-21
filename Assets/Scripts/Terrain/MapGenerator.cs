dcab3fcfad2bb744869f84557ef61cb2ac5f5bab
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2022-11-21 13:00:47 -0600

    3D Terrain generation with different levels of detail

--- Assets/Scripts/Terrain/MapGenerator.cs
+++ Assets/Scripts/Terrain/MapGenerator.cs
@@ -18,38 +18,40 @@ namespace Terrain
             NoiseMap,
             ColorMap,
             Mesh
-        };
+        }
 
-
-        [SerializeField] private int mapWidth;
-        [SerializeField] private int mapHeight;
+        [SerializeField] private int chunkSize = 241;
         [SerializeField] private float noiseScale;
         [SerializeField] private int octaves;
         [Range(0, 1)] [SerializeField] private float persistance;
         [SerializeField] private float lacunarity;
         [SerializeField] private int seed;
         [SerializeField] private Vector2 offset;
+        [SerializeField] private float heightMultiplier;
+        [SerializeField] private AnimationCurve meshHeightCurve;
 
         public bool autoUpdate;
         public TerrainType[] regions;
         public DrawMode drawMode;
+        [Range(0, 6)]
+        public int levelOfDetail;
 
         public void GenerateMap()
         {
-            float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance,
+            float[,] noiseMap = Noise.GenerateNoiseMap(chunkSize, chunkSize, noiseScale, octaves, persistance,
                 lacunarity, seed, offset);
 
-            Color[] colorMap = new Color[mapHeight * mapWidth];
-            for (int y = 0; y < mapHeight; y++)
+            Color[] colorMap = new Color[chunkSize * chunkSize];
+            for (int y = 0; y < chunkSize; y++)
             {
-                for (int x = 0; x < mapWidth; x++)
+                for (int x = 0; x < chunkSize; x++)
                 {
                     float currentHeight = noiseMap[x, y];
                     for (int i = 0; i < regions.Length; i++)
                     {
                         if (currentHeight <= regions[i].height)
                         {
-                            colorMap[y * mapWidth + x] = regions[i].color;
+                            colorMap[y * chunkSize + x] = regions[i].color;
                             break;
                         }
                     }
@@ -63,27 +65,22 @@ namespace Terrain
             }
             else if (drawMode == DrawMode.ColorMap)
             {
-                display.DrawMapTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+                display.DrawMapTexture(TextureGenerator.TextureFromColorMap(colorMap, chunkSize, chunkSize));
             }
             else if (drawMode == DrawMode.Mesh)
             {
-                display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap),
-                    TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+                display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, heightMultiplier, meshHeightCurve, levelOfDetail),
+                    TextureGenerator.TextureFromColorMap(colorMap, chunkSize, chunkSize));
             }
+        }
+
+        private void Start()
+        {
+            GenerateMap();
         }
 
         private void OnValidate()
         {
-            if (mapHeight < 1)
-            {
-                mapHeight = 1;
-            }
-
-            if (mapWidth < 1)
-            {
-                mapWidth = 1;
-            }
-
             if (octaves < 0)
             {
                 octaves = 0;
